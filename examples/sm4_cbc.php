18037afa2bbb8dc8f706557f9b6b98805a33fa82
Zhi Guan <guanzhi1980@gmail.com>
2023-09-09 18:05:48 +0800

    Add constants

--- examples/sm4_cbc.php
+++ examples/sm4_cbc.php
@@ -1,9 +1,10 @@
 <?php
 print("GMSSL_SM4_KEY_SIZE = ".GMSSL_SM4_KEY_SIZE."\n");
 print("GMSSL_SM4_BLOCK_SIZE = ".GMSSL_SM4_BLOCK_SIZE."\n");
+print("GMSSL_SM4_CBC_IV_SIZE = ".GMSSL_SM4_CBC_IV_SIZE."\n");
 
 $key = gmssl_rand_bytes(GMSSL_SM4_KEY_SIZE);
-$iv = gmssl_rand_bytes(GMSSL_SM4_BLOCK_SIZE);
+$iv = gmssl_rand_bytes(GMSSL_SM4_CBC_IV_SIZE);
 $plaintext = "abc";
 
 $ciphertext = gmssl_sm4_cbc_encrypt($key, $iv, $plaintext);
--- examples/sm4_ctr.php
+++ examples/sm4_ctr.php
@@ -1,9 +1,10 @@
 <?php
 print("GMSSL_SM4_KEY_SIZE = ".GMSSL_SM4_KEY_SIZE."\n");
 print("GMSSL_SM4_BLOCK_SIZE = ".GMSSL_SM4_BLOCK_SIZE."\n");
+print("GMSSL_SM4_CTR_IV_SIZE = ".GMSSL_SM4_CTR_IV_SIZE."\n");
 
 $key = gmssl_rand_bytes(GMSSL_SM4_KEY_SIZE);
-$iv = gmssl_rand_bytes(GMSSL_SM4_BLOCK_SIZE);
+$iv = gmssl_rand_bytes(GMSSL_SM4_CTR_IV_SIZE);
 $plaintext = "abc";
 
 $ciphertext = gmssl_sm4_ctr_encrypt($key, $iv, $plaintext);
